21ebaba2a0cc295ab0f39f2a21c60b4e5b2a4d39
Divyanshu Tyagi <divyanshutyagi8@gmail.com>
2019-03-13 12:13:55 +0530

    ML workspace updated

--- Machine Learning Workspace/Model Test.r
+++ Machine Learning Workspace/Model Test.r
@@ -1,7 +1,7 @@
 library(e1071)
 ins_train <- iris[c(1:40, 51:90, 101:140),]
-ins_test <- iris[c(41:50,91:100,141:150),c(1:3)]
+ins_test <- iris[c(41:50,91:100,141:150),]
 model <- naiveBayes(ins_train[,-5],ins_train[,5])
-my_prediction <- predict(model, ins_test)
+my_prediction <- predict(model, ins_test[,-5])
 model
 table(my_prediction)
