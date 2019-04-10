8e654973db195d3f4f53e45bff9b17d594e9baae
creatorgaming <divyanshutyagi8@gmail.com>
2019-04-10 20:19:05 +0530

    AI test update for ssh key

--- Artificial Intelligence/Q13-Pallindrome.pl
+++ Artificial Intelligence/Q13-Pallindrome.pl
@@ -1,11 +1,15 @@
-reversal([],[]) :- !.
-reversal([X],[X]) :- !.
-reversal([H|T],L) :- reversal(T, R),
-                     append(R, [H], L).
+reversal([], []) :-
+    !.
+reversal([X], [X]) :-
+    !.
+reversal([H|T], L) :-
+    reversal(T, R),
+    append(R, [H], L).
 
-go:- 
-  write('Enter List : '),
-  read(L),
-  pallindrome(L).
+go :-
+    write('Enter List to check pallindrome : '),
+    read(L),
+    pallindrome(L).
 
-pallindrome(L1) :- reversal(L1,L1).
+pallindrome(L1) :-
+    reversal(L1, L1).
